2683cbc891c3255b0dc9ba11ce18b963f2d50587
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-10-21 01:24:27 +0000

    moving files
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude_plugins@2809 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- simple_panels/setup.py
+++ simple_panels/setup.py
@@ -1,13 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='motmot.fview.fview2panels',
-      namespace_packages = ['motmot','motmot.fview'],
-
+setup(name='simple_panels',
       version='0.1',
-      entry_points = {
-    'motmot.fview.plugins':'motmot.fview.fview2panels = motmot.fview.fview2panels:FView2Panels_Class',
-    },
-
       packages = find_packages(),
-      package_data = {'motmot.fview.fview2panels':['fview2panels.xrc']},
       )
