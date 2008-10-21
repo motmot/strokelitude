721066880c4459c51071a0427d6afb08a733beca
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-10-21 03:11:50 +0000

    it appears to work
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude_plugins@2811 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- simple_panels/setup.py
+++ simple_panels/setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+import os, sys
+
+libfname='libdumpframe1.so' # XXX change on Windows and Mac
+fname =os.path.join('simple_panels',libfname)
+if not os.path.exists(fname):
+    # XXX should only do this check during build phase
+    sys.stderr.write('Error: could not find %s. Run scons. Quitting.\n'%fname)
+    sys.exit(1)
 
 setup(name='simple_panels',
       version='0.1',
       packages = find_packages(),
+      package_data={'simple_panels':libfname},
       )
