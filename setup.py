f1632e90cd9e48d777309cfddd1eabd7a1b15831
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-11-07 23:09:29 +0000

    renaming
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude@2922 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='motmot.fview.strokelitude',
-      namespace_packages = ['motmot','motmot.fview'],
-
+setup(name='strokelitude',
       version='0.1',
       entry_points = {
-    'motmot.fview.plugins':'motmot.fview.strokelitude = motmot.fview.strokelitude:StrokelitudeClass',
+    'motmot.fview.plugins':'strokelitude = strokelitude:StrokelitudeClass',
     },
 
       packages = find_packages(),
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       zip_safe=True,
-      package_data = {'motmot.fview.strokelitude':['fview_strokelitude.xrc']},
+      package_data = {'strokelitude':['strokelitude.xrc']},
       )
