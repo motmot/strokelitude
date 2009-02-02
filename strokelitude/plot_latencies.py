ed39f1afdc91cadc16862496ed4e2326c680b5e5
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2009-02-02 19:29:39 +0000

    fview_strokelitude: plot IFIs
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude@3278 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- strokelitude/plot_latencies.py
+++ strokelitude/plot_latencies.py
@@ -9,7 +9,21 @@ stim=h5.root.stimulus_timeseries_queue[:]
 stroke_data =h5.root.stroke_data[:]
 
 fig=plt.figure()
-ax1=fig.add_subplot(2,1,1)
+ax1=fig.add_subplot(3,1,1)
+
+
+for name in ['trigger_timestamp',
+             'receive_timestamp',
+             'start_display_timestamp',
+             'stop_display_timestamp']:
+    arr = stim[name]
+    ifi = arr[1:]-arr[:-1]
+    ifi = ifi[ ifi != 0 ]
+    ax1.plot(ifi*1000.0,'o',label=name,mew=0,ms=3)
+ax1.set_ylabel('IFI (msec)')
+ax1.legend()
+
+ax1=fig.add_subplot(3,1,2)
 
 latencies_sec = {}
 for name in ['receive_timestamp',
@@ -20,7 +34,7 @@ for name in ['receive_timestamp',
 
     ax1.plot(stim['trigger_timestamp'],
              1000.0*latencies_sec[name],
-             '.',
+             'o',mew=0,ms=3,
              label=name)
 
 latencies_sec['image_analysis'] = (stroke_data['processing_timestamp']-
@@ -33,7 +47,7 @@ ax1.set_xlabel('time (sec)')
 ax1.set_ylabel('latency (msec)')
 ax1.legend()
 
-ax2=fig.add_subplot(2,1,2)
+ax2=fig.add_subplot(3,1,3)
 bins = np.linspace(0,100,30)
 for name,results in latencies_sec.iteritems():
     ax2.hist(results*1000.0,bins=bins,alpha=0.5,label=name)
