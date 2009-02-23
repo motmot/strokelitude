9c892d76371845eb4c9db90d03302594bf5ecb9d
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2009-02-23 19:29:22 +0000

    fview_strokelitude: improve plot
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude@3336 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- plot_raw_timeseries.py
+++ plot_raw_timeseries.py
@@ -5,6 +5,8 @@ import sys
 import tables
 import fview_ext_trig.easy_decode as easy_decode
 
+import matplotlib.ticker as mticker
+
 if 1:
 
     fname = sys.argv[1]
@@ -12,7 +14,6 @@ if 1:
 
     stroke_data=h5.root.stroke_data[:]
     stroke_times = stroke_data['trigger_timestamp']
-    stroke_diff = stroke_data['right']-stroke_data['left']
 
     time_data=h5.root.time_data[:]
     gain,offset,resids = easy_decode.get_gain_offset_resids(
@@ -38,7 +39,7 @@ if 1:
     analog_gain = Vcc/ADCmax
 
     t0 = r['timestamps'][0]
-    N_subplots = len(chans)+3
+    N_subplots = len(chans)+2
     ax=None
     for i in range(N_subplots):
         ax = pylab.subplot(N_subplots,1,i+1,sharex=ax)
@@ -48,16 +49,14 @@ if 1:
             ax.set_ylabel('V')
             ax.legend()
         elif i == len(chans):
-            ax.plot(stroke_times-t0,stroke_diff,label='R-L')
-            ax.set_ylabel('R-L (degrees)')
-            ax.legend()
-        elif i == len(chans)+1:
-            ax.plot(stroke_times-t0,90-stroke_data['right'],label='R')
+            ax.plot(stroke_times-t0,stroke_data['right'],label='R')
             ax.set_ylabel('R (degrees)')
             ax.legend()
-        elif i == len(chans)+2:
-            ax.plot(stroke_times-t0,90-stroke_data['left'],label='L')
+        elif i == len(chans)+1:
+            ax.plot(stroke_times-t0,stroke_data['left'],label='L')
             ax.set_ylabel('L (degrees)')
             ax.legend()
+        ax.xaxis.set_major_formatter(mticker.FormatStrFormatter("%s"))
+        ax.yaxis.set_major_formatter(mticker.FormatStrFormatter("%s"))
     ax.set_xlabel('Time (sec)')
     pylab.show()
