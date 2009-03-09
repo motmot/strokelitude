caae8eb738220d2343245127c0ea4f319331fefd
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2009-03-09 02:34:36 +0000

    fview_strokelitude: as used for experiment today
    git-svn-id: https://code.astraw.com/kookaburra/trunk/fview_strokelitude_plugins@3366 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- stripe/example_experiments/replay1.py
+++ stripe/example_experiments/replay1.py
@@ -1,15 +1,16 @@
-normal_gain = 0
+normal_gain = -20
 
 # initialize with defined background conditions
-stripe_control.closed_loop( gain=normal_gain, offset=0.0, duration_sec=1 )
+stripe_control.closed_loop( gain=normal_gain, offset=0.0, duration_sec=10 )
 
-# initialize with defined background conditions
-stripe_control.closed_loop( gain=normal_gain,
-                            offset=SinusoidalBias( freq_hz  = 0.1,
-                                                   amplitude= 200, # deg/sec
-                                                   ),
-                            save_sequence=True,
-                            duration_sec=30 )
+for i in range(5):
+    # initialize with defined background conditions
+    stripe_control.closed_loop( gain=normal_gain,
+                                offset=SinusoidalBias( freq_hz  = 0.1,
+                                                       amplitude= 200, # deg/sec
+                                                       ),
+                                save_sequence=True,
+                                duration_sec=30 )
 
-stripe_control.replay_sequence()
-
+    for j in range(5):
+        stripe_control.replay_sequence()
