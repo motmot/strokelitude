0304909caba1c2159d2454f77bb7d7bfecca3ef8
Andrew Straw <strawman@astraw.com>
2009-06-29 12:45:47 -0700

    add --timestamps option to plot_raw_timeseries

--- strokelitude/plot_raw_timeseries.py
+++ strokelitude/plot_raw_timeseries.py
@@ -7,9 +7,15 @@ import tables
 import motmot.fview_ext_trig.easy_decode as easy_decode
 
 import matplotlib.ticker as mticker
+from optparse import OptionParser
+import pytz, datetime, time
+pacific = pytz.timezone('US/Pacific')
 
-if 1:
+def format_date(x, pos=None):
+    return str(datetime.datetime.fromtimestamp(x,pacific))
 
+def doit(fname,options):
+    print 'options.timestamps',options.timestamps
     fname = sys.argv[1]
     h5 = tables.openFile(fname,mode='r')
 
@@ -39,7 +45,28 @@ if 1:
     ADCmax = (2**10)-1
     analog_gain = Vcc/ADCmax
 
+    n_adc_samples = len(r['timestamps'])
+    dt = r['timestamps'][1]-r['timestamps'][0]
+    samps_per_sec = 1.0/dt
+    adc_duration = n_adc_samples*dt
+    print '%d samples at %.1f samples/sec = %.1f seconds'%(n_adc_samples,
+                                                           samps_per_sec,
+                                                           adc_duration)
     t0 = r['timestamps'][0]
+    stroke_times_zero_offset = stroke_times-t0
+    if len(stroke_times_zero_offset):
+        stroke_data_duration = stroke_times_zero_offset[-1]
+        total_duration = max(stroke_data_duration,adc_duration)
+    else:
+        total_duration = adc_duration
+
+    if options.timestamps:
+        t_offset = 0
+        t_plot_start = t0
+    else:
+        t_offset = t0
+        t_plot_start = 0
+
     N_subplots = len(chans)+2
     ax=None
     for i in range(N_subplots):
@@ -50,19 +77,43 @@ if 1:
             except Exception, err:
                 print 'ERROR: ingnoring exception %s'%(err,)
                 label = 'channel %s'%chans[i]
-            ax.plot(r['timestamps']-t0,r[chans[i]]*analog_gain,
+            ax.plot(r['timestamps']-t_offset,r[chans[i]]*analog_gain,
                     label=label)
             ax.set_ylabel('V')
             ax.legend()
         elif i == len(chans):
-            ax.plot(stroke_times-t0,stroke_data['right'],label='R')
+            ax.plot(stroke_times-t_offset,stroke_data['right'],label='R')
             ax.set_ylabel('R (degrees)')
             ax.legend()
         elif i == len(chans)+1:
-            ax.plot(stroke_times-t0,stroke_data['left'],label='L')
+            ax.plot(stroke_times-t_offset,stroke_data['left'],label='L')
             ax.set_ylabel('L (degrees)')
             ax.legend()
-        ax.xaxis.set_major_formatter(mticker.FormatStrFormatter("%s"))
+        if options.timestamps:
+            ax.xaxis.set_major_formatter(
+                mticker.FuncFormatter(format_date))
+        else:
+            ax.xaxis.set_major_formatter(mticker.FormatStrFormatter("%s"))
         ax.yaxis.set_major_formatter(mticker.FormatStrFormatter("%s"))
     ax.set_xlabel('Time (sec)')
+    ax.set_xlim((t_plot_start,t_plot_start+total_duration))
+    if options.timestamps:
+        pylab.gcf().autofmt_xdate()
+
     pylab.show()
+
+def main():
+    usage = '%prog [options] FILE'
+
+    parser = OptionParser(usage)
+
+    parser.add_option("--timestamps", action='store_true',
+                      default=False)
+
+    (options, args) = parser.parse_args()
+    fname = args[0]
+    doit(fname,options)
+
+if __name__=='__main__':
+    main()
+
